bc4db49f02165b21db5e2d5052e47369c7cb7337
AlexandreGlatz <alexandre.glatzpro@gmail.com>
2025-03-09 23:20:50 +0100

    [ADD] Bricks color change

--- Bricks/Assets/Scripts/Bricks.cs
+++ Bricks/Assets/Scripts/Bricks.cs
@@ -1,10 +1,13 @@
 using UnityEngine;
 using UnityEngine.UIElements;
+using static System.Net.WebRequestMethods;
 
 public class Bricks : MonoBehaviour
 {
     [SerializeField] GameObject _gameobject;
     [SerializeField] Vector3 _grid;
+    Color _currentColor;
+    Material _material;
     bool _isPlaced;
     public bool IsPlaced
     {
@@ -18,16 +21,26 @@ public class Bricks : MonoBehaviour
         set { _grid = value; }
     }
 
+    public Color CurrentColor
+    {
+        get { return _currentColor; }
+        set { _currentColor = value; }
+    }
+
     void Start()
     {
+        _currentColor = Color.white;
         _isPlaced = false;
-        _gameobject.GetComponent<Renderer>().material.shader = Shader.Find("Shader Graphs/Unplaced Brick");
+        _material = _gameobject.GetComponent<Renderer>().material;
+        _material.shader = Shader.Find("Shader Graphs/Unplaced Brick");
     }
 
     // Update is called once per frame
     void Update()
     {
         if(_isPlaced == true) { return; }
+
+        _material.SetColor("_BaseColor", _currentColor);
 
         if (Input.GetKeyDown(KeyCode.D))
         {
@@ -37,7 +50,6 @@ public class Bricks : MonoBehaviour
         {
             _gameobject.transform.position += new Vector3(- _grid.x, 0.0f, 0.0f);
         }
-
 
         if (Input.GetKeyDown(KeyCode.S))
         {
--- Bricks/Assets/Scripts/BricksManager.cs
+++ Bricks/Assets/Scripts/BricksManager.cs
@@ -5,9 +5,12 @@ public class BricksManager : MonoBehaviour
     [SerializeField] GameObject _brickPrefab;
     GameObject _currentBrick;
     float _currentY;
+    Color[] _palette = { Color.white, Color.red, Color.blue, Color.green, Color.yellow };
+    int _index;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        _index = 0;
         _currentY = 0.0f;
         _currentBrick = Instantiate(_brickPrefab, new Vector3(0.0f, _currentY, 0.0f), Quaternion.identity);
     }
@@ -15,13 +18,35 @@ public class BricksManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Bricks brick = _currentBrick.GetComponent<Bricks>();
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            Bricks brick = _currentBrick.GetComponent<Bricks>();
             brick.IsPlaced = true;
+
+            Material material = _currentBrick.GetComponent<Renderer>().material;
+            material.shader = Shader.Find("Universal Render Pipeline/Lit");
+            material.SetColor("_Base Map", brick.CurrentColor);
+
             _currentY = _currentBrick.transform.position.y + brick.Grid.y;
-            _currentBrick.GetComponent<Renderer>().material.shader = Shader.Find("Universal Render Pipeline/Lit");
             _currentBrick = Instantiate(_brickPrefab, new Vector3(_currentBrick.transform.position.x, _currentY, _currentBrick.transform.position.z), Quaternion.identity);
+        }
+
+        if(Input.GetKeyDown(KeyCode.I)) 
+        {
+            _index++;
+            if (_index >= _palette.Length)
+                _index = 0;
+
+            brick.CurrentColor = _palette[_index];
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            _index--;
+            if (_index < 0)
+                _index = _palette.Length - 1;
+
+            brick.CurrentColor = _palette[_index];
         }
     }
 }
